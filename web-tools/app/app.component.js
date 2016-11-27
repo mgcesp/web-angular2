0f107384c3607fddaa21e3e4f2d32a18385022bf
Manuel <mgcesp@gmail.com>
2016-11-26 19:40:57 -0800

    Added web-tools projects

---
+++ web-tools/app/app.component.js
@@ -0,0 +1,27 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var child_component_1 = require('./inputs-and-outputs/child.component');
+var AppComponent = (function () {
+    function AppComponent() {
+    }
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'my-app',
+            template: "\n\t<h1>Inputs and Outputs</h1>\n\t<h2>Parent Component</h2>\n\t<label>Enter ParentComponent value:</label>\n\t<input type=\"text\" #ptext (keyup)=\"0\">\n\t<p>Value from child component</p>\n\t{{childData}}\n\t<child-component (childEvent)=\"childData=$event\" [parentData]=\"ptext.value\"></child-component>\n\t",
+            directives: [child_component_1.ChildComponent]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
